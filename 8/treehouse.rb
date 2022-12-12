b4c06bf999580370daf72b0921f7a59ba76efb62
Phillip Strom <phillip.strom@gmail.com>
2022-12-12 11:48:07 -0700

    Solve part 2

--- 8/treehouse.rb
+++ 8/treehouse.rb
@@ -37,6 +37,50 @@ class Treehouse
     return true if grid[(y+1)..-1].all? { |row| row[x] < height }
     false
   end
+  
+  
+  def highest_interior_tree_score
+    highest_score = 0
+    grid.each_with_index do |row, y_index|
+      next if y_index == 0 || y_index == grid.size - 1
+      row.each_with_index do |height, x_index|
+        next if x_index == 0 || x_index == grid[y_index].size - 1
+        
+        # compare in each direction all shorter trees 
+        current_score = tree_score(height, y_index, x_index)
+        highest_score = current_score if current_score > highest_score
+      end
+    end
+    highest_score
+  end
+  
+  def tree_score(height, y, x)
+    # navigate left
+    left = 0
+    grid[y][0..(x-1)].reverse.each { |tree| 
+      left += 1
+      break if tree >= height 
+    }
+    # navigate right
+    right = 0
+    grid[y][(x+1)..-1].each { |tree|
+      right += 1 
+      break if tree >= height 
+     }
+    # navigate up
+    up = 0
+    grid[0..(y-1)].reverse.each { |row|
+      up += 1
+      break if row[x] >= height 
+     }
+    # navigate down
+    down = 0
+    grid[(y+1)..-1].each { |row|
+      down += 1
+      break if row[x] >= height 
+     }
+    left * right * up * down
+  end
 end
 
 
--- 8/treehouse_test.rb
+++ 8/treehouse_test.rb
@@ -19,8 +19,23 @@ class TreehouseTest < Minitest::Test
     forest = Treehouse.new('test_input.txt')
     assert_equal 5, forest.interior_visible_tree_count
   end
+  
+  def test_it_can_calc_tree_score
+    forest = Treehouse.new('test_input.txt')
+    assert_equal 4, forest.tree_score(5, 1, 2)
+    assert_equal 8, forest.tree_score(5, 3, 2)
+  end
+  
+  def test_it_can_find_highest_interior_tree_score
+    forest = Treehouse.new('test_input.txt')
+    assert_equal 8, forest.highest_interior_tree_score
+  end
 end
 
 # Solve Part 1:
 forest = Treehouse.new('input.txt')
 puts "Part 1 Solution: #{forest.visible_tree_count}"
+
+# Solve Part 2:
+forest = Treehouse.new('input.txt')
+puts "Part 2 Solution: #{forest.highest_interior_tree_score}"
